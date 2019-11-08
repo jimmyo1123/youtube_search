62c1802bb16491878bb40e9ac7847e1823e4bf86
Jimmy Chen <enjoy8062@gmail.com>
2019-11-08 20:06:39 +1030

    Updates

---
+++ precache-manifest.bdeb9382bf78c11e919ee5bdc84f35cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3602bd64fe8135f7cb7521f25382287",
+    "url": "/youtube_search/index.html"
+  },
+  {
+    "revision": "ce4600de96997999a9c0",
+    "url": "/youtube_search/static/css/main.03c6ca28.chunk.css"
+  },
+  {
+    "revision": "2d0751566b0f234c2e94",
+    "url": "/youtube_search/static/js/2.f1b9daee.chunk.js"
+  },
+  {
+    "revision": "ce4600de96997999a9c0",
+    "url": "/youtube_search/static/js/main.3cbecef1.chunk.js"
+  },
+  {
+    "revision": "8d1ad5347ffd96d5f49d",
+    "url": "/youtube_search/static/js/runtime-main.f5cc49d5.js"
+  }
+]);
